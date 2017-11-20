6064fcd8f84f6d9f0414dea97ce2f1c65504275b
SHINING <jack7770126@gmail.com>
2017-11-20 23:12:39 +0800

    [advance] add the ajax btn.

--- advance/task12/01/main.js
+++ advance/task12/01/main.js
@@ -1,8 +1,21 @@
-window.onload = function(){
-  var xhr = new XMLHttpRequest()
-  xhr.open('GET', '/category.json', true)
-  xhr.send()
-  xhr.onload = function(data){
-    console.log(data)
-  }
+window.onload = function () {
+  var btn = document.querySelector('#more')
+  btn.addEventListener('click', function (event) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', '/HungerValleyCode/advance/task12/01/category.json', true)
+    xhr.send()
+    xhr.addEventListener('load', function (data) {
+      if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+        var data = res.data.data
+        var ul = document.querySelector('#contentList')
+        data.forEach(element => {
+          var li = document.createElement('li')
+          li.innerHTML = element.name
+          ul.appendChild(li)
+        });
+      } else {
+        alert('服务器出错啦')
+      }
+    })
+  })
 }
