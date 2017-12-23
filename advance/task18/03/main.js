723a88fe48dfec5a759419b002638c1e26a067e9
SHINING <jack7770126@gmail.com>
2017-12-24 02:40:26 +0800

    [advance] finish task18 03.

---
+++ advance/task18/03/main.js
@@ -0,0 +1,490 @@
+const _data = {
+  "totalHits": 500,
+  "hits": [{
+    "previewHeight": 71,
+    "likes": 56,
+    "favorites": 23,
+    "tags": "swans, proud swans, pride",
+    "webformatHeight": 304,
+    "views": 944,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 20,
+    "downloads": 494,
+    "pageURL": "https://pixabay.com/en/swans-proud-swans-pride-waterfowl-3028727/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/19/21/16/swans-3028727_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3072ff6063ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 4500,
+    "user_id": 2364555,
+    "user": "pixel2013",
+    "type": "photo",
+    "id": 3028727,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/08/20/10-10-37-579_250x250.jpg",
+    "imageHeight": 2140
+  }, {
+    "previewHeight": 99,
+    "likes": 37,
+    "favorites": 28,
+    "tags": "christmas, presents, kado",
+    "webformatHeight": 426,
+    "views": 10441,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 7,
+    "downloads": 5386,
+    "pageURL": "https://pixabay.com/en/christmas-presents-kado-winter-3026685/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/18/18/37/christmas-3026685_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3092efc043ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 5456,
+    "user_id": 2218222,
+    "user": "Nietjuh",
+    "type": "photo",
+    "id": 3026685,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/12/18/20-40-06-48_250x250.jpg",
+    "imageHeight": 3632
+  }, {
+    "previewHeight": 99,
+    "likes": 38,
+    "favorites": 14,
+    "tags": "away, bridge, web",
+    "webformatHeight": 426,
+    "views": 1864,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 17,
+    "downloads": 1449,
+    "pageURL": "https://pixabay.com/en/away-bridge-web-wood-nature-3024773/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/17/19/08/away-3024773_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30b2ff3023ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 6000,
+    "user_id": 1032521,
+    "user": "Tama66",
+    "type": "photo",
+    "id": 3024773,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/09/28/19-45-08-506_250x250.jpeg",
+    "imageHeight": 4000
+  }, {
+    "previewHeight": 97,
+    "likes": 41,
+    "favorites": 16,
+    "tags": "swan, proud to be a swan, pride",
+    "webformatHeight": 415,
+    "views": 856,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 9,
+    "downloads": 653,
+    "pageURL": "https://pixabay.com/en/swan-proud-to-be-a-swan-pride-3028659/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/19/20/34/swan-3028659_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3072ef1083ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 4500,
+    "user_id": 2364555,
+    "user": "pixel2013",
+    "type": "photo",
+    "id": 3028659,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/08/20/10-10-37-579_250x250.jpg",
+    "imageHeight": 2918
+  }, {
+    "previewHeight": 101,
+    "likes": 35,
+    "favorites": 20,
+    "tags": "model, artemis fauna, nude",
+    "webformatHeight": 432,
+    "views": 2290,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 7,
+    "downloads": 402,
+    "pageURL": "https://pixabay.com/en/model-artemis-fauna-nude-woman-3026201/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/18/14/07/model-3026201_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3092af4003ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 7701,
+    "user_id": 6078268,
+    "user": "Studio-Dee",
+    "type": "photo",
+    "id": 3026201,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/12/21/02-38-47-523_250x250.jpg",
+    "imageHeight": 5206
+  }, {
+    "previewHeight": 79,
+    "likes": 32,
+    "favorites": 10,
+    "tags": "gift, christmas, surprise",
+    "webformatHeight": 340,
+    "views": 2788,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 15,
+    "downloads": 1326,
+    "pageURL": "https://pixabay.com/en/gift-christmas-surprise-packed-3030279/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/20/16/49/gift-3030279_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b20f2af3083ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 6000,
+    "user_id": 1564471,
+    "user": "annca",
+    "type": "photo",
+    "id": 3030279,
+    "userImageURL": "https://cdn.pixabay.com/user/2015/11/27/06-58-54-609_250x250.jpg",
+    "imageHeight": 3190
+  }, {
+    "previewHeight": 95,
+    "likes": 36,
+    "favorites": 13,
+    "tags": "swan, proud to be a swan, pride",
+    "webformatHeight": 408,
+    "views": 658,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 8,
+    "downloads": 527,
+    "pageURL": "https://pixabay.com/en/swan-proud-to-be-a-swan-pride-3028673/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/19/20/39/swan-3028673_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3072ef3023ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 4443,
+    "user_id": 2364555,
+    "user": "pixel2013",
+    "type": "photo",
+    "id": 3028673,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/08/20/10-10-37-579_250x250.jpg",
+    "imageHeight": 2833
+  }, {
+    "previewHeight": 99,
+    "likes": 32,
+    "favorites": 12,
+    "tags": "christmas, father christmas, merry christmas",
+    "webformatHeight": 426,
+    "views": 11605,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 15,
+    "downloads": 3875,
+    "pageURL": "https://pixabay.com/en/christmas-father-christmas-3027132/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/18/23/53/christmas-3027132_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30829f7033ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 2508,
+    "user_id": 3583171,
+    "user": "mary1826",
+    "type": "photo",
+    "id": 3027132,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/05/13/03-59-10-397_250x250.jpg",
+    "imageHeight": 1672
+  }, {
+    "previewHeight": 63,
+    "likes": 26,
+    "favorites": 10,
+    "tags": "woman, eyes, face",
+    "webformatHeight": 273,
+    "views": 217,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 11,
+    "downloads": 142,
+    "pageURL": "https://pixabay.com/en/woman-eyes-face-close-eyelashes-3030681/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/20/21/25/woman-3030681_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b20f2efc003ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 4225,
+    "user_id": 2364555,
+    "user": "pixel2013",
+    "type": "photo",
+    "id": 3030681,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/08/20/10-10-37-579_250x250.jpg",
+    "imageHeight": 1807
+  }, {
+    "previewHeight": 105,
+    "likes": 31,
+    "favorites": 12,
+    "tags": "christmas, christmas tree, background",
+    "webformatHeight": 452,
+    "views": 3667,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 5,
+    "downloads": 964,
+    "pageURL": "https://pixabay.com/en/christmas-christmas-tree-background-3029496/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/20/08/30/christmas-3029496_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3062cfd073ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 3508,
+    "user_id": 9301,
+    "user": "geralt",
+    "type": "photo",
+    "id": 3029496,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/06/05/17-13-06-921_250x250.jpg",
+    "imageHeight": 2480
+  }, {
+    "previewHeight": 99,
+    "likes": 32,
+    "favorites": 18,
+    "tags": "christmas, christmas gift, christmas present",
+    "webformatHeight": 426,
+    "views": 6720,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 3,
+    "downloads": 3017,
+    "pageURL": "https://pixabay.com/en/christmas-christmas-gift-3024428/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/17/15/42/christmas-3024428_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30b2cf6093ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 5184,
+    "user_id": 4014469,
+    "user": "TerriC",
+    "type": "photo",
+    "id": 3024428,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/02/17/14-09-51-26_250x250.jpg",
+    "imageHeight": 3456
+  }, {
+    "previewHeight": 99,
+    "likes": 24,
+    "favorites": 18,
+    "tags": "christmas, presents, kado",
+    "webformatHeight": 426,
+    "views": 3941,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 5,
+    "downloads": 1879,
+    "pageURL": "https://pixabay.com/en/christmas-presents-kado-winter-3026688/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/18/18/38/christmas-3026688_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3092efc093ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 5456,
+    "user_id": 2218222,
+    "user": "Nietjuh",
+    "type": "photo",
+    "id": 3026688,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/12/18/20-40-06-48_250x250.jpg",
+    "imageHeight": 3632
+  }, {
+    "previewHeight": 99,
+    "likes": 32,
+    "favorites": 7,
+    "tags": "mountains, hut, s",
+    "webformatHeight": 426,
+    "views": 1038,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 5,
+    "downloads": 723,
+    "pageURL": "https://pixabay.com/en/mountains-hut-s-alpine-alm-3026990/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/18/22/08/mountains-3026990_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30921fd013ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 5850,
+    "user_id": 7435999,
+    "user": "thomaszbinden",
+    "type": "photo",
+    "id": 3026990,
+    "userImageURL": "",
+    "imageHeight": 3900
+  }, {
+    "previewHeight": 98,
+    "likes": 20,
+    "favorites": 12,
+    "tags": "hopelessness, despair, not",
+    "webformatHeight": 420,
+    "views": 171,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 8,
+    "downloads": 83,
+    "pageURL": "https://pixabay.com/en/hopelessness-despair-not-suicide-3028909/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/19/23/04/hopelessness-3028909_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30721f4083ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 4332,
+    "user_id": 686414,
+    "user": "Alexas_Fotos",
+    "type": "photo",
+    "id": 3028909,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/12/11/08-42-48-680_250x250.png",
+    "imageHeight": 2843
+  }, {
+    "previewHeight": 99,
+    "likes": 28,
+    "favorites": 4,
+    "tags": "lago di limides, dolomites, alpine",
+    "webformatHeight": 425,
+    "views": 1662,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 11,
+    "downloads": 1146,
+    "pageURL": "https://pixabay.com/en/lago-di-limides-dolomites-alpine-3025780/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/18/09/54/lago-di-limides-3025780_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30a2ffc013ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 2050,
+    "user_id": 4371822,
+    "user": "Julius_Silver",
+    "type": "photo",
+    "id": 3025780,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/11/30/23-14-18-786_250x250.jpg",
+    "imageHeight": 1364
+  }, {
+    "previewHeight": 119,
+    "likes": 18,
+    "favorites": 9,
+    "tags": "doll figures, figures, wooden figures",
+    "webformatHeight": 511,
+    "views": 427,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 9,
+    "downloads": 218,
+    "pageURL": "https://pixabay.com/en/doll-figures-figures-wooden-figures-3028918/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/19/23/24/doll-figures-3028918_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30721f5093ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 4077,
+    "user_id": 1195798,
+    "user": "Couleur",
+    "type": "photo",
+    "id": 3028918,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/12/17/13-47-07-302_250x250.jpg",
+    "imageHeight": 3258
+  }, {
+    "previewHeight": 85,
+    "likes": 19,
+    "favorites": 8,
+    "tags": "portugal, algarve, benagil",
+    "webformatHeight": 364,
+    "views": 409,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 5,
+    "downloads": 182,
+    "pageURL": "https://pixabay.com/en/portugal-algarve-benagil-caves-3029665/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/20/10/48/portugal-3029665_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b3062ef2043ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 2200,
+    "user_id": 4371822,
+    "user": "Julius_Silver",
+    "type": "photo",
+    "id": 3029665,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/11/30/23-14-18-786_250x250.jpg",
+    "imageHeight": 1254
+  }, {
+    "previewHeight": 81,
+    "likes": 30,
+    "favorites": 22,
+    "tags": "football, shoe, football boot",
+    "webformatHeight": 348,
+    "views": 6114,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 15,
+    "downloads": 3050,
+    "pageURL": "https://pixabay.com/en/football-shoe-football-boot-sport-3024154/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/17/12/45/football-3024154_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30b29f1053ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 6000,
+    "user_id": 51581,
+    "user": "Comfreak",
+    "type": "photo",
+    "id": 3024154,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/10/11/13-29-32-37_250x250.jpg",
+    "imageHeight": 3265
+  }, {
+    "previewHeight": 99,
+    "likes": 18,
+    "favorites": 8,
+    "tags": "the star of bethlehem, flower, christmas",
+    "webformatHeight": 426,
+    "views": 723,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 15,
+    "downloads": 522,
+    "pageURL": "https://pixabay.com/en/the-star-of-bethlehem-flower-3023791/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/17/07/29/the-star-of-bethlehem-3023791_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30c2ffd003ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 5184,
+    "user_id": 5975425,
+    "user": "_Alicja_",
+    "type": "photo",
+    "id": 3023791,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/11/29/21-12-13-226_250x250.jpg",
+    "imageHeight": 3456
+  }, {
+    "previewHeight": 99,
+    "likes": 20,
+    "favorites": 7,
+    "tags": "the sea, the waterfall, ocean waves",
+    "webformatHeight": 426,
+    "views": 1294,
+    "webformatWidth": 640,
+    "previewWidth": 150,
+    "comments": 4,
+    "downloads": 1027,
+    "pageURL": "https://pixabay.com/en/the-sea-the-waterfall-ocean-waves-3025268/",
+    "previewURL": "https://cdn.pixabay.com/photo/2017/12/18/01/41/the-sea-3025268_150.jpg",
+    "webformatURL": "https://pixabay.com/get/ea35b30a2af2093ed95c4518b7494e93e57fe7dd04b0144091f0c478a0ebb6_640.jpg",
+    "imageWidth": 5760,
+    "user_id": 7201644,
+    "user": "Quangpraha",
+    "type": "photo",
+    "id": 3025268,
+    "userImageURL": "https://cdn.pixabay.com/user/2017/11/28/12-45-12-473_250x250.jpg",
+    "imageHeight": 3840
+  }],
+  "total": 562998
+}
+
+/**
+ * 1. 获取数据
+ * 2. 计算可视宽
+ * 3. 计算单行图片摆放数量
+ * 4. 单行图片渲染，拉伸填满行
+ */
+
+function Casks(options) {
+  this.parentNode = options.parentNode
+  this.basicHeight = options.basicHeight
+  this.rowList = []
+  this.totalWidth = 0
+}
+
+Casks.prototype.handleData = function (data) {
+  this.parentNode.innerHTML = ''
+  this.parentWidth = parseFloat((this.parentNode).offsetWidth) - 20
+  for (let i in data) {
+    data[i].newHeight = this.basicHeight
+    data[i].newWidth = data[i].webformatWidth * (this.basicHeight / data[i].webformatHeight)
+
+    if (this.totalWidth + data[i].newWidth > this.parentWidth) {
+      this.render(this.rowList)
+    } else {
+      this.rowList.push(data[i])
+      this.totalWidth += data[i].newWidth
+    }
+  }
+  this.render(this.rowList)
+}
+
+Casks.prototype.render = function () {
+  let rate = this.parentWidth / this.totalWidth
+  let newBasicHeight = this.basicHeight * rate
+  for (let i in this.rowList) {
+    let div = document.createElement('div')
+    let img = `<img class="img-item" src="${this.rowList[i].webformatURL}" alt="${this.rowList[i].tags}">`
+    div.classList.add('img-wrap')
+    div.innerHTML = img
+    div.style.height = newBasicHeight + 'px'
+    div.style.width = this.rowList[i].newWidth * rate + 'px'
+    this.parentNode.appendChild(div)
+  }
+  this.totalWidth = 0
+  this.rowList = []
+}
+
+window.onload = function () {
+  let options = {
+    parentNode: document.querySelector('.img-list'),
+    basicHeight: 200
+  }
+  let newCask = new Casks(options)
+  newCask.handleData(_data.hits)
+
+  let timer
+  window.addEventListener('resize', () => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      console.log(111)
+      newCask.handleData(_data.hits)
+    }, 200)
+  })
+}
